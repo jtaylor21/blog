d2a6d92a4df0c38ad3ec9d77bf1ffe27a1d6ab1c
vcabansag <vcabansag@gmail.com>
2011-11-18 09:30:42 -0600

    s3 fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "640x640>" },
                     :storage => :s3,
-                    :bucket => Setting.find_by_name("s3_bucket").value,
+                    :bucket => Setting.find_by_name('s3_bucket').value,
                     :s3_credentials => {
-                      :access_key_id => Setting.find_by_name("s3_access_key_id").value,
-                      :secret_access_key => Setting.find_by_name("s3_secret_access_key").value
+                      :access_key_id => Setting.find_by_name('s3_access_key_id').value,
+                      :secret_access_key => Setting.find_by_name('s3_secret_access_key').value
                     }
 
 end
