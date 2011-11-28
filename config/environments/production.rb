0772c2bd8947221f8482a53b1273cc04931f880f
vcabansag <vcabansag@gmail.com>
2011-11-27 21:33:43 -0600

    Added Tweet constant in production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,3 +58,7 @@ Blog::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
+
+module Settings
+  TwitterHandle = "vcabansag"
+end
