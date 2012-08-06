6534077aacc9afbad0e6892c5af731566db8a4c4
Vincent Cabansag <vcabansag@gmail.com>
2012-08-06 12:32:07 -0500

    Setting schedule to run only in production

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,13 +1,16 @@
 require 'rufus/scheduler'
-scheduler = Rufus::Scheduler.start_new
 
-urls = %w[http://google.com http://amazon.com http://facebook.com http://apple.com]
+if Rails.env.production?
+  scheduler = Rufus::Scheduler.start_new
 
-time = [2, 7, 18, 21]
+  urls = %w[http://google.com http://amazon.com http://facebook.com http://apple.com]
 
-scheduler.every "#{time.sample}m" do
-   require "net/http"
-   require "uri"
-   url = urls.shuffle.take(1)
-   Net::HTTP.get_response(URI.parse(url))
+  time = [2, 7, 18, 21]
+
+  scheduler.every "#{time.sample}m" do
+     require "net/http"
+     require "uri"
+     url = urls.shuffle.take(1)
+     Net::HTTP.get_response(URI.parse(url))
+  end
 end
