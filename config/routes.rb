23cc35ce02e0c45deb303ba86c11e4c9119100fb
Vincent Cabansag <vcabansag@gmail.com>
2013-04-21 15:00:32 -0500

    Adding legacy routes to support later

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,17 @@ Blog::Application.routes.draw do
 
   match "*path" => redirect('/')
 
+  # Legacy routes
+  # http://vincentcabansag.com/posts/13/networking-events-for-code-academy-students
+  # http://vincentcabansag.com/posts/12/lessons-from-code-academy-students
+  # http://vincentcabansag.com/posts/11/seventh-week-of-code-academy
+  # http://vincentcabansag.com/posts/8/configuring-the-paperclip-gem
+  # http://vincentcabansag.com/posts/10/sixth-week-of-code-academy
+  # http://vincentcabansag.com/posts/7/fifth-week-of-code-academy
+  # http://vincentcabansag.com/posts/6/fourth-week-of-code-academy
+  # http://vincentcabansag.com/posts/5/third-week-of-code-academy
+  # http://vincentcabansag.com/posts/4/second-week-of-code-academy
+  # http://vincentcabansag.com/posts/3/first-week-of-code-academy
+  # http://vincentcabansag.com/posts/2/hello-world%21
+
 end
