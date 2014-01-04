709a47675ee83847ea4fe475d3893e168ea90e6d
Vincent Cabansag <vcabansag@gmail.com>
2014-01-03 23:53:43 -0600

    Trying something with text in the controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @posts = Post.all.order('created_at DESC')[0..2]
+    @greeting = "I'm Vincent Cabansag, an entreprenuer and developer. I help make the <a href='http://www.starterleague.com'>Starter League</a> the best place to learn and work. I teach at the <a href='http://www.starterschool.com'>Starter School</a>. And I also like to take <a href='http://instagram.com/vcabansag'>photos</a>."
   end
 
 end
