66f67c6e240f65f344d15c4f3eafce1c9b211e8a
Vincent Cabansag <vcabansag@gmail.com>
2014-06-05 15:47:46 -0500

    Copy edit home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @posts = Post.all.order('created_at DESC')[0..3]
-    @greeting = "I'm Vincent Cabansag, an entreprenuer and developer. I help make the <a href='http://www.starterleague.com'>Starter League</a> the best place to learn and work. I teach at the <a href='http://www.starterschool.com'>Starter School</a>. And I also like to take <a href='http://instagram.com/vcabansag'>photos</a>."
+    @greeting = "I'm Vincent Cabansag, an entreprenuer and developer. I help make the <a href='http://www.starterleague.com' title='Starter League'>Starter League</a> the best place to learn and work. I teach at <a href='http://www.starterschool.com' title='Starter School'>Starter School</a>. And I also like to take <a href='http://instagram.com/vcabansag' title='vcabansag on Instagram'>photos</a>."
   end
 
 end
