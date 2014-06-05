16c43f2e3d5d69c66a9facfa1636e91e0a92d54d
Vincent Cabansag <vcabansag@gmail.com>
2014-06-05 15:37:24 -0500

    Increased latest posts to 4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-    @posts = Post.all.order('created_at DESC')[0..2]
+    @posts = Post.all.order('created_at DESC')[0..3]
     @greeting = "I'm Vincent Cabansag, an entreprenuer and developer. I help make the <a href='http://www.starterleague.com'>Starter League</a> the best place to learn and work. I teach at the <a href='http://www.starterschool.com'>Starter School</a>. And I also like to take <a href='http://instagram.com/vcabansag'>photos</a>."
   end
 
