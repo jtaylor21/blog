a6d615f5fc63d3c694eeee89b0e74d597428a169
Vincent Cabansag <vcabansag@gmail.com>
2015-07-26 03:55:50 -0500

    Showing 5 posts on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-    @posts = Post.all.order('created_at DESC')[0..3]
+    @posts = Post.all.order('created_at DESC')[0..4]
     @greeting = "I'm Vincent Cabansag, an entreprenuer and developer. I help make the <a href='http://www.starterleague.com' title='Starter League'>Starter League</a> the best place to learn and work. I teach at <a href='http://www.starterschool.com' title='Starter School'>Starter School</a>. And I also like to take <a href='http://instagram.com/vcabansag' title='vcabansag on Instagram'>photos</a>."
   end
 
